0e8151d60ce548d3561e8265c5b3d06567a952dc
Selam Degefu <selam.tadd.degefu@gmail.com>
2021-04-05 17:15:36 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result
+  if(ride < 400){
+    result= 'This one is on me!'
+  }
+  else if (ride > 2000 && ride <2500){
+  result =  'I will gladly take your thirty bucks.'
+  }
+
+  else if(ride > 2500){
+    result = 'No can do.'
+  }
+    return result
+}
+scuberGreetingForFeet(5000)
+function ternaryCheckCity(city){
+  if (city === 'NYC'){
+    return "Ok, sounds good."
+  }
+  else{
+    return 'No go.'
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+  switch (tip){
+    case 'generous':
+    return "Thank you so much.";
+    break;
+     case 'not as generous':
+      return "Thank you."
+       break;
+       default:
+        return 'Bye.'
+         break;
+  }
+  }
+ switchOnCharmFromTip('generous')
